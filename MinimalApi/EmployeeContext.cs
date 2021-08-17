209d3a68513a07503c7ff843dfcfd241b4225eed
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2021-08-17 19:59:19 +1200

    Add using C# 10 File Scoped Namespaces

--- MinimalApi/EmployeeContext.cs
+++ MinimalApi/EmployeeContext.cs
@@ -1,18 +1,17 @@
-﻿namespace MinimalApi
+﻿namespace MinimalApi;
+
+public class EmployeeContext : DbContext
 {
-    public class EmployeeContext : DbContext
+    public EmployeeContext(DbContextOptions options) : base(options)
     {
-        public EmployeeContext(DbContextOptions options) : base(options)
-        {
-        }
-
-        public DbSet<Employee> Employees { get; set; }
     }
 
-    public class Employee
-    {
-        public int Id { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+}
 
-        public string Name { get; set; }
-    }
+public class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
 }
