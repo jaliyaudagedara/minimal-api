44538c1cd519c53e7a0886deba573fd5f2ac42a0
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2023-03-16 12:58:41 +1300

    Update .NET packages to 8.0.0-preview.2.23128.3

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -2,12 +2,12 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // ConfigureServices
 builder.Services.AddDbContext<EmployeeContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minimal API", Description = "OpenAPI specification for Minimal API", Version = "v1" });
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minimal API", Description = "OpenAPI specification for Minimal API", Version = "v1" });
 });
 
 WebApplication app = builder.Build();
@@ -15,69 +15,71 @@ WebApplication app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minimal API V1");
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minimal API V1");
 });
 
 // Configure
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+	app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/employees", async (EmployeeContext dbContext) =>
+RouteGroupBuilder employeesRoute = app.MapGroup("/employees");
+
+employeesRoute.MapGet("/", async (EmployeeContext dbContext) =>
 {
-    return TypedResults.Ok(await dbContext.Employees.ToListAsync());
+	return TypedResults.Ok(await dbContext.Employees.ToListAsync());
 });
 
-app.MapGet("/employees/{id}", async Task<Results<Ok<Employee>, NotFound>> (int id, EmployeeContext dbContext) =>
+employeesRoute.MapGet("/{id}", async Task<Results<Ok<Employee>, NotFound>> (int id, EmployeeContext dbContext) =>
 {
-    Employee employee = await dbContext.Employees.FindAsync(id);
-    if (employee is null)
-    {
-        return TypedResults.NotFound();
-    }
+	Employee employee = await dbContext.Employees.FindAsync(id);
+	if (employee is null)
+	{
+		return TypedResults.NotFound();
+	}
 
-    return TypedResults.Ok(employee);
+	return TypedResults.Ok(employee);
 });
 
-app.MapPost("/employees", async (Employee employee, EmployeeContext dbContext) =>
+employeesRoute.MapPost("/", async (Employee employee, EmployeeContext dbContext) =>
 {
-    await dbContext.Employees.AddAsync(employee);
-    await dbContext.SaveChangesAsync();
+	await dbContext.Employees.AddAsync(employee);
+	await dbContext.SaveChangesAsync();
 
-    return TypedResults.Created($"/employees/{employee.Id}", employee);
+	return TypedResults.Created($"/employees/{employee.Id}", employee);
 });
 
-app.MapPut("/employees/{id}", async Task<Results<NoContent, BadRequest, NotFound>> (int id, Employee employee, EmployeeContext dbContext) =>
+employeesRoute.MapPut("/{id}", async Task<Results<NoContent, BadRequest, NotFound>> (int id, Employee employee, EmployeeContext dbContext) =>
 {
-    if (id != employee.Id)
-    {
-        return TypedResults.BadRequest();
-    }
+	if (id != employee.Id)
+	{
+		return TypedResults.BadRequest();
+	}
 
-    if (!await dbContext.Employees.AnyAsync(x => x.Id == id))
-    {
-        return TypedResults.NotFound();
-    }
+	if (!await dbContext.Employees.AnyAsync(x => x.Id == id))
+	{
+		return TypedResults.NotFound();
+	}
 
-    dbContext.Entry(employee).State = EntityState.Modified;
-    await dbContext.SaveChangesAsync();
+	dbContext.Entry(employee).State = EntityState.Modified;
+	await dbContext.SaveChangesAsync();
 
-    return TypedResults.NoContent();
+	return TypedResults.NoContent();
 });
 
-app.MapDelete("/employees/{id}", async Task<Results<NoContent, NotFound>> (int id, EmployeeContext dbContext) =>
+employeesRoute.MapDelete("/{id}", async Task<Results<NoContent, NotFound>> (int id, EmployeeContext dbContext) =>
 {
-    Employee employee = await dbContext.Employees.FindAsync(id);
-    if (employee is null)
-    {
-        return TypedResults.NotFound();
-    }
+	Employee employee = await dbContext.Employees.FindAsync(id);
+	if (employee is null)
+	{
+		return TypedResults.NotFound();
+	}
 
-    dbContext.Employees.Remove(employee);
-    await dbContext.SaveChangesAsync();
+	dbContext.Employees.Remove(employee);
+	await dbContext.SaveChangesAsync();
 
-    return TypedResults.NoContent();
+	return TypedResults.NoContent();
 });
 
 await app.RunAsync();
